1821be2d51277a0c2d0ab0688136c2386397cc7f
yuanzhou <yuanzhou19@gmail.com>
2021-04-14 14:06:02 -0400

    Update dataset dois

--- src/datacite_dois.py
+++ src/datacite_dois.py
@@ -21,53 +21,127 @@ DATACITE_CLIENT_ID = ''
 DATACITE_CLIENT_PASSWORD = ''
 DATACITE_API_URL = 'https://api.test.datacite.org/dois '
 HUBMAP_PREFIX = '10.80478'
+ENTITY_API_URL = 'https://entity-api.test.hubmapconsortium.org/'
 
-json_to_post = {
-  "data": {
-    "id": dataset['hubmap_id'],
-    "type": "dois",
-    "attributes": {
-      "event": "publish",
-      "doi": f"{HUBMAP_PREFIX}/{dataset['hubmap_id']}",
-      "creators": dataset['creators'],
-      "titles": [{
-        "title": dataset['title']
-      }],
-      "publisher": "DataCite e.V.",
-      "publicationYear": 2016,
-      "types": {
-        "resourceTypeGeneral": "Text"
-      },
-      "url": "https://schema.datacite.org/meta/kernel-4.0/index.html",
-      "schemaVersion": "http://datacite.org/schema/kernel-4"
+
+"""
+Register the given dataset with DataCite 
+
+Parameters
+----------
+dataset: dict
+    The dataset dict to be registered
+user_token: str
+    The user's globus nexus token
+
+Returns
+-------
+list
+    The list of new ids dicts, the number of dicts is based on the count
+"""
+def register_dataset(dataset, user_token):
+    if ('entity_type' in dataset) and (dataset['entity_type'] == 'Dataset'):
+        json_to_post = {
+          "data": {
+            "id": dataset['hubmap_id'],
+            "type": "dois",
+            "attributes": {
+              "event": "publish",
+              "doi": f"{HUBMAP_PREFIX}/{dataset['hubmap_id']}",
+              "creators": dataset['creators'],
+              "titles": [{
+                "title": dataset['title']
+              }],
+              "publisher": "DataCite e.V.",
+              "publicationYear": 2016,
+              "types": {
+                "resourceTypeGeneral": "Text"
+              },
+              "url": "https://schema.datacite.org/meta/kernel-4.0/index.html",
+              "schemaVersion": "http://datacite.org/schema/kernel-4"
+            }
+          }
+        }
+
+        request_headers = {
+            'Content-Type': 'application/vnd.api+json'
+        }
+
+        # Make the request using Basic Auth
+        # Disable ssl certificate verification
+        response = requests.post(url = DATACITE_API_URL, auth = HTTPBasicAuth(DATACITE_CLIENT_ID, DATACITE_CLIENT_PASSWORD), headers = request_headers, json = json_to_post, verify = False) 
+
+        # Invoke .raise_for_status(), an HTTPError will be raised with certain status codes
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            logger.info("======registered DOI via DataCite======")
+
+            # Get the response json
+            doi_data = response.json()
+
+            dataset_uuid = dataset['uuid']
+
+            try:
+                update_dataset_doi_info(dataset_uuid, doi_data, user_token)
+            except requests.exceptions.RequestException as e:
+                # Bubble up the error
+                raise requests.exceptions.RequestException(e)
+        else:
+            msg = f"Unable to register DOIs via DataCite" 
+            
+            # Log the full stack trace, prepend a line with our message
+            logger.exception(msg)
+
+            logger.debug("======status code from DataCite======")
+            logger.debug(response.status_code)
+
+            logger.debug("======response text from DataCite======")
+            logger.debug(response.text)
+
+            # Also bubble up the error message from DataCite
+            raise requests.exceptions.RequestException(response.text)
+
+"""
+Update the dataset's doi properties
+
+Parameters
+----------
+dataset_uuid: str
+    The dataset uuid
+doi_data: dict
+    The DataCite generated doi information
+user_token: str
+    The user's globus nexus token
+"""
+def update_dataset_doi_info(dataset_uuid, doi_data, user_token):
+    # Update the registered_doi, doi_url, and has_doi properties after DOI creatiion
+    dataset_properties_to_update = {
+        'registered_doi': doi_data['attributes']['doi'],
+        'doi_url': doi_data['attributes']['url'],
+        'has_doi': True
     }
-  }
-}
 
-request_headers = {
-    'Content-Type': 'application/vnd.api+json'
-}
+    request_headers = {
+        'Authorization': f"Bearer {user_token}",
+        'X-Hubmap-Application': 'ingest-api'
+    }
 
-# Make the request using Basic Auth
-# Disable ssl certificate verification
-response = requests.post(url = DATACITE_API_URL, auth = HTTPBasicAuth(DATACITE_CLIENT_ID, DATACITE_CLIENT_PASSWORD), headers = request_headers, json = json_to_post, verify = False, params = query_parms) 
-
-# Invoke .raise_for_status(), an HTTPError will be raised with certain status codes
-response.raise_for_status()
-
-if response.status_code == 200:
-    logger.info("======registered DOI via DataCite======")
-else:
-    msg = f"Unable to register DOIs via DataCite" 
+    response = requests.put(url = f"{ENTITY_API_URL}/entities/{dataset_uuid}", headers = request_headers, json = dataset_properties_to_update, verify = False) 
     
-    # Log the full stack trace, prepend a line with our message
-    logger.exception(msg)
+    if response.status_code == 200:
+        logger.info("======The target entity has been updated with DOI info======")
+    else:
+        msg = f"Unable to update the DOI properties of dataset {dataset_uuid}" 
+        
+        # Log the full stack trace, prepend a line with our message
+        logger.exception(msg)
 
-    logger.debug("======status code from DataCite======")
-    logger.debug(response.status_code)
+        logger.debug("======status code from entity-api======")
+        logger.debug(response.status_code)
 
-    logger.debug("======response text from DataCite======")
-    logger.debug(response.text)
+        logger.debug("======response text from entity-api======")
+        logger.debug(response.text)
 
-    # Also bubble up the error message from DataCite
-    raise requests.exceptions.RequestException(response.text)
+        # Also bubble up the error message from entity-api
+        raise requests.exceptions.RequestException(response.text)
