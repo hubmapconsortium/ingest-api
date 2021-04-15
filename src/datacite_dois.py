180d90ba7ba54cbbecaa938a13f37b551c610d51
yuanzhou <yuanzhou19@gmail.com>
2021-04-14 22:21:35 -0400

    Runnable python script for doi

--- src/datacite_dois.py
+++ src/datacite_dois.py
@@ -1,6 +1,8 @@
+import os
+import sys
 import requests
 import logging
-
+from flask import Flask
 from requests.auth import HTTPBasicAuth
 
 # Suppress InsecureRequestWarning warning when requesting status on https with ssl cert verify disabled
@@ -41,14 +43,11 @@ def register_dataset(dataset, user_token, datacite_repository_id, datacite_repos
               'event': 'publish',
               'doi': f"{datacite_hubmap_prefix}/{dataset['hubmap_id']}",
               # 'creators': dataset['creators'] if ('creators' in dataset) else [],
-              # 'titles': [{
-              #   'title': dataset['title'] if ('title' in dataset) else 'Default title placeholder'
-              # }],
               'creators': [{
                 'name': "DataCite Metadata Working Group"
               }],
               'titles': [{
-                'title': "DataCite Metadata Schema Documentation for the Publication and Citation of Research Data v4.0"
+                'title': dataset['title'] if ('title' in dataset) else 'Default title placeholder'
               }],
               'publisher': 'HuBMAP Consortium',
               'publicationYear': 2021,
@@ -77,17 +76,21 @@ def register_dataset(dataset, user_token, datacite_repository_id, datacite_repos
         # Invoke .raise_for_status(), an HTTPError will be raised with certain status codes
         #response.raise_for_status()
 
-        if response.status_code == 200:
+        # Don't forget 201
+        if response.status_code in [200, 201]:
             logger.info("======registered DOI via DataCite======")
-
+   
             # Get the response json of resulting doi
-            doi_data = response.json()
+            doi_data = response.json()['data']
+
+            logger.debug("======resulting json from DataCite======")
+            logger.debug(doi_data)
 
             dataset_uuid = dataset['uuid']
 
             try:
-                # Update the doi properties via entity-api
-                update_dataset_doi_info(dataset_uuid, doi_data, user_token, entity_api_url)
+                # Update the dataset properties via entity-api
+                update_dataset_after_doi_created(dataset_uuid, doi_data, user_token, entity_api_url)
             except requests.exceptions.RequestException as e:
                 # Bubble up the error
                 raise requests.exceptions.RequestException(e)
@@ -102,14 +105,11 @@ def register_dataset(dataset, user_token, datacite_repository_id, datacite_repos
 
             logger.debug("======response text from DataCite======")
             logger.debug(response.text)
-
-            # Also bubble up the error message from DataCite
-            raise requests.exceptions.RequestException(response.text)
     else:
         raise KeyError('The entity_type of the given dataset is not Dataset')
 
 """
-Update the dataset's doi properties
+Update the dataset's properties after doi is created
 
 Parameters
 ----------
@@ -120,8 +120,9 @@ doi_data: dict
 user_token: str
     The user's globus nexus token
 """
-def update_dataset_doi_info(dataset_uuid, doi_data, user_token, entity_api_url):
+def update_dataset_after_doi_created(dataset_uuid, doi_data, user_token, entity_api_url):
     # Update the registered_doi, doi_url, and has_doi properties after DOI creatiion
+    # Are we also supposed to update the Dataset.status to "Published"?
     dataset_properties_to_update = {
         'registered_doi': doi_data['attributes']['doi'],
         'doi_url': doi_data['attributes']['url'],
@@ -151,3 +152,63 @@ def update_dataset_doi_info(dataset_uuid, doi_data, user_token, entity_api_url):
 
         # Also bubble up the error message from entity-api
         raise requests.exceptions.RequestException(response.text)
+
+
+# Running this python file as a script
+# python3 datacite.py <user_token> <dataset_uuid>
+if __name__ == "__main__":
+    try:
+        user_token = sys.argv[1]
+
+        try:
+            dataset_uuid = sys.argv[2]
+        except IndexError as e:
+            msg = "Missing dataset uuid argument"
+            # Log the full stack trace, prepend a line with our message
+            logger.exception(msg)
+            sys.exit(msg)  
+    except IndexError as e:
+        msg = "Missing user token argument"
+        # Log the full stack trace, prepend a line with our message
+        logger.exception(msg)
+        sys.exit(msg)
+
+    # Specify the absolute path of the instance folder and use the config file relative to the instance path
+    app = Flask(__name__, instance_path=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance'), instance_relative_config=True)
+    app.config.from_pyfile('app.cfg')
+
+    datacite_repository_id = app.config['DATACITE_REPOSITORY_ID']
+    datacite_repository_password = app.config['DATACITE_REPOSITORY_PASSWORD']
+    datacite_api_url = app.config['DATACITE_API_URL']
+    datacite_hubmap_prefix = app.config['DATACITE_HUBMAP_PREFIX']
+    entity_api_url = app.config['ENTITY_WEBSERVICE_URL']
+
+    target_url = f"{entity_api_url}/entities/{dataset_uuid}"
+
+    auth_header = {
+        'Authorization': f"Bearer {user_token}"
+    }
+
+    response = requests.get(url = target_url, headers = auth_header,  verify = False) 
+
+    if response.status_code == 200:
+
+        dataset = response.json()
+
+        logger.debug(dataset)
+
+        try:
+            register_dataset(dataset, user_token, datacite_repository_id, datacite_repository_password, datacite_api_url, datacite_hubmap_prefix, entity_api_url)
+        except requests.exceptions.RequestException as e:
+            logger.exception(e)
+    else:
+        msg = f"Unable to query the target dataset with uuid: {dataset_uuid}" 
+        
+        # Log the full stack trace, prepend a line with our message
+        logger.exception(msg)
+
+        logger.debug("======status code from entity-api======")
+        logger.debug(response.status_code)
+
+        logger.debug("======response text from entity-api======")
+        logger.debug(response.text)
