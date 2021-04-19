6b5554be88e7728966e45112ac7606fafcdbfba1
yuanzhou <yuanzhou19@gmail.com>
2021-04-19 14:30:55 -0400

    Remove unused config

--- src/dataset_helper.py
+++ src/dataset_helper.py
@@ -59,7 +59,6 @@ def load_flask_instance_config():
     app.config.from_pyfile('app.cfg')
 
     # Remove trailing slash / from URL base to avoid "//" caused by config with trailing slash
-    app.config['DATACITE_API_URL'] = app.config['DATACITE_API_URL'].strip('/')
     app.config['ENTITY_WEBSERVICE_URL'] = app.config['ENTITY_WEBSERVICE_URL'].strip('/')
     app.config['SEARCH_WEBSERVICE_URL'] = app.config['SEARCH_WEBSERVICE_URL'].strip('/')
 
