0ea274966a224c315f1b41d1eab1582aa81c5432
ChuckKollar <chuck@kollar.com>
2021-07-01 14:48:11 -0400

    Revert "Used EntityApi in main"
    This reverts commit 1a91266e849a67e26f5a31c092d37162474efbeb.

--- src/dataset_helper_object.py
+++ src/dataset_helper_object.py
@@ -280,9 +280,17 @@ if __name__ == "__main__":
         sys.exit(msg)
 
     dataset_helper = DatasetHelper()
-    entity_api = EntityApi(user_token ,_entity_api_url)
 
-    response = entity_api.get_entities(dataset_uuid)
+    auth_header = {
+        'Authorization': f"Bearer {user_token}"
+    }
+
+    response = requests.get(
+        url=f"{_entity_api_url}/entities/{dataset_uuid}",
+        headers=auth_header,
+        verify=False
+    )
+
     if response.status_code == 200:
         dataset = response.json()
 
