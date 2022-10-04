70ddaeb36bd1a589b72ee4b5923c75f810b77c74
ChuckKollar <chuck@kollar.com>
2022-10-04 13:43:32 -0400

    Moved file* endpoints to Blueprint routes

--- src/app_utils/request_validation/__init__.py
+++ src/app_utils/request_validation/__init__.py
@@ -1,5 +1,5 @@
-from flask import abort
 import logging
+from app_utils.error import bad_request_error
 
 logger: logging.Logger = logging.getLogger(__name__)
 
@@ -14,13 +14,3 @@ def require_json(request):
     if not request.is_json:
         bad_request_error("A json body and appropriate Content-Type header are required")
 
-
-def bad_request_error(err_msg):
-    """
-    Throws error for 400 Bad Reqeust with message
-    Parameters
-    ----------
-    err_msg : str
-        The custom error message to return to end users
-    """
-    abort(400, description=err_msg)
