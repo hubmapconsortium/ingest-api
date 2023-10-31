fc43c45563455cd00f7da8bdd9b9439a2d4dbad1
ChuckKollar <chuck@kollar.com>
2023-10-31 13:27:07 -0400

    Removed commented code

--- src/routes/validation/__init__.py
+++ src/routes/validation/__init__.py
@@ -43,9 +43,6 @@ __all__ = ["ingest_validation_tools_upload",
            "ingest_validation_tools_table_validator"
            ]
 
-# sys.path.pop()
-# sys.path.pop()
-
 
 validation_blueprint: Blueprint = Blueprint('validation', __name__)
 
