0a7fcbbe0c054d46bd0791b973324f9bdcb203a9
Juan Puerto <=>
2024-01-18 16:49:26 -0500

    General: Changes to add dataset-type to values returned from rule chain, add dataset-type to old assay_types.yaml to map old data_types to new dataset_type, add dag_ids for processed datasets to the "alt-names" as well

--- src/routes/assayclassifier/rule_generator.py
+++ src/routes/assayclassifier/rule_generator.py
@@ -140,11 +140,12 @@ def main() -> None:
         all_assay_types = ([canonical_name]
                            + [elt for elt in type_dict.get('alt-names', [])
                               if isinstance(elt, str)])
-        quoted_assay_types = ["'" + tp + "'" for tp in all_assay_types]
+        quoted_assay_types = [f"'{tp}'" for tp in all_assay_types]
         vitessce_hints = type_dict.get('vitessce-hints', [])
         description = type_dict.get('description', '')
         is_primary = type_dict['primary']
         contains_pii = "true" if type_dict.get('contains-pii', "") else "false"
+        dataset_type = type_dict.get("dataset-type", "")
 
         # Provide a special hint for datasets to be vis-lifted
         if 'pyramid' in vitessce_hints or 'publication_ancillary' in all_assay_types:
@@ -224,7 +225,8 @@ def main() -> None:
                            f" 'vitessce-hints': {vitessce_hints},"
                            f" 'contains-pii': {contains_pii},"
                            f" 'primary': true,"
-                           f" 'description': '{description}'}}"
+                           f" 'description': '{description}',"
+                           f" 'dataset-type': '{dataset_type}' }}"
                            ),
                  "rule_description": f"non-DCWG primary {canonical_name}"
                  }
@@ -264,6 +266,7 @@ def main() -> None:
                        f" 'description': '{description}',"
                        f" 'contains-pii': true,"
                        f" 'primary': true,"
+                       f" 'dataset-type': '{data_type}',"
                        f" 'must-contain': [{must_contain_str}]"
                        "}"
                        ),
@@ -287,6 +290,7 @@ def main() -> None:
                        f" 'description': '{description}',"
                        f" 'contains-pii': true,"
                        f" 'primary': true,"
+                       f" 'dataset-type': '{data_type}',"
                        f" 'must-contain': [{must_contain_str}]"
                        "}"
                        ),
@@ -332,6 +336,7 @@ def main() -> None:
                        f" 'dir-schema': '{schema}',"
                        f" 'contains-pii': true,"
                        f" 'primary': true,"
+                       f" 'dataset-type': '{data_type}',"
                        f" 'description': '{description}'"
                        "}"
                        ),
@@ -355,6 +360,7 @@ def main() -> None:
                        f" 'dir-schema': '{schema}',"
                        f" 'contains-pii': true,"
                        f" 'primary': true,"
+                       f" 'dataset-type': '{data_type}',"
                        f" 'description': '{description}'"
                        "}"
                        ),
@@ -375,6 +381,7 @@ def main() -> None:
                    " 'dir-schema': 'atacseq-v2',"
                    f" 'contains-pii': true,"
                    f" 'primary': true,"
+                   f" 'dataset-type': 'ATACseq',"
                    f" 'description': 'sciATAC-seq'"
                    "}"
                    ),
@@ -398,6 +405,7 @@ def main() -> None:
                        f" 'dir-schema': '{schema}',"
                        f" 'contains-pii': false,"
                        f" 'primary': true,"
+                       f" 'dataset-type': 'Histology',"
                        f" 'description': '{description}'"
                        "}"
                        ),
@@ -437,6 +445,7 @@ def main() -> None:
                        f" 'dir-schema': '{schema}',"
                        f" 'contains-pii': false,"
                        f" 'primary': true,"
+                       f" 'dataset-type': '{data_type}',"
                        f" 'description': '{description}'"
                        "}"
                        ),
