ce4d92cb78202c9721b166b25011c128690c148d
DerekFurstPitt <drf57@pitt.edu>
2024-01-22 00:56:16 -0500

    updated tests to reflect changes to verify_dataset_title_info. No longer need to mock assayname as this is no longer called.

--- test/dataset_helper_object/test_verify_dataset_title_info.py
+++ test/dataset_helper_object/test_verify_dataset_title_info.py
@@ -19,27 +19,15 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
 
         # For a "Dataset": response.json() from requests.get(url = f"{_entity_api_url}/entities/{dataset_uuid}", ...)
         self.dataset_uuid = '12345678-1234-5678-1234-567812345678'
-        self.data_types = ['bulk-RNA', 'IMC']
-        self.dataset = {'uuid': self.dataset_uuid, 'data_types': self.data_types}
+        self.dataset_type = ['RNAseq', 'MIBI']
+        self.dataset = {'uuid': self.dataset_uuid, 'dataset_type': self.dataset_type}
         self.user_token = "fake token"
 
     @patch('dataset_helper_object.requests.get')
     @patch('dataset_helper_object.EntitySdk.get_entity_by_id')
     @patch('dataset_helper_object.EntitySdk.get_ancestors')
-    @patch('dataset_helper_object.SearchSdk.assayname')
-    def test_verify_dataset_title_info_happy_path(self, mock_assayname, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
-        # https://github.com/hubmapconsortium/search-api/blob/main/src/search-schema/data/definitions/enums/assay_types.yaml
+    def test_verify_dataset_title_info_happy_path(self, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
         def resp1():
-            response_dict = {'description': 'Imaging Mass Cytometry', 'alt-names': [], 'primary': 'true', 'vitessce-hints': []}
-            return response_dict
-
-        def resp2():
-            response_dict = {'description': 'Bulk RNA-seq', 'alt-names': [], 'primary': 'true', 'vitessce-hints': []}
-            return response_dict
-
-        mock_assayname.side_effect = [resp1(), resp2()]
-
-        def resp3():
             dataset1 = hubmap_sdk.Dataset({'entity_type': 'Sample',
                                            'specimen_type': 'Organ',
                                            'sample_category': 'organ',
@@ -56,14 +44,14 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
             entity = [dataset1, dataset2]
             return entity
 
-        mock_get_ancestors.side_effect = [resp3()]
+        mock_get_ancestors.side_effect = [resp1()]
 
-        def resp4():
+        def resp2():
             dataset = hubmap_sdk.Dataset(self.dataset)
             return dataset
-        mock_get_entity_by_id.side_effect = [resp4()]
+        mock_get_entity_by_id.side_effect = [resp2()]
 
-        def resp5():
+        def resp3():
             r = requests.Response()
             json_data = '''
             {
@@ -75,12 +63,11 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
             '''
             r._content = json_data.encode('utf-8')
             return r
-        mock_url_get.side_effect = [resp5()]
+        mock_url_get.side_effect = [resp3()]
 
         result = self.dataset_helper.verify_dataset_title_info(self.dataset_uuid, self.user_token)
 
         self.assertEqual(len(result), 0)
-        mock_assayname.assert_called()
         mock_get_ancestors.assert_called()
         mock_get_entity_by_id.assert_called()
         mock_url_get.assert_called()
@@ -98,21 +85,8 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
     @patch('dataset_helper_object.requests.get')
     @patch('dataset_helper_object.EntitySdk.get_entity_by_id')
     @patch('dataset_helper_object.EntitySdk.get_ancestors')
-    @patch('dataset_helper_object.SearchSdk.assayname')
-    def test_verify_dataset_title_info_organ_code_description_not_found(self, mock_assayname, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
-        # https://github.com/hubmapconsortium/search-api/blob/main/src/search-schema/data/definitions/enums/assay_types.yaml
+    def test_verify_dataset_title_info_organ_code_description_not_found(self, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
         def resp1():
-            response_dict = {'description': 'Imaging Mass Cytometry', 'alt-names': [], 'primary': 'true',
-                             'vitessce-hints': []}
-            return response_dict
-
-        def resp2():
-            response_dict = {'description': 'Bulk RNA-seq', 'alt-names': [], 'primary': 'true', 'vitessce-hints': []}
-            return response_dict
-
-        mock_assayname.side_effect = [resp1(), resp2()]
-
-        def resp3():
             dataset1 = hubmap_sdk.Dataset({'entity_type': 'Sample',
                                            'specimen_type': 'Organ',
                                            'sample_category': 'organ',
@@ -129,14 +103,14 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
             entity = [dataset1, dataset2]
             return entity
 
-        mock_get_ancestors.side_effect = [resp3()]
+        mock_get_ancestors.side_effect = [resp1()]
 
-        def resp4():
+        def resp2():
             dataset = hubmap_sdk.Dataset(self.dataset)
             return dataset
-        mock_get_entity_by_id.side_effect = [resp4()]
+        mock_get_entity_by_id.side_effect = [resp2()]
 
-        def resp5():
+        def resp3():
             r = requests.Response()
             json_data = '''
             {
@@ -148,12 +122,11 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
             '''
             r._content = json_data.encode('utf-8')
             return r
-        mock_url_get.side_effect = [resp5()]
+        mock_url_get.side_effect = [resp3()]
 
         result = self.dataset_helper.verify_dataset_title_info(self.dataset_uuid, self.user_token)
 
         self.assertEqual(len(result), 0)
-        mock_assayname.assert_called()
         mock_get_ancestors.assert_called()
         mock_get_entity_by_id.assert_called()
         mock_url_get.assert_called()
@@ -161,19 +134,8 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
     @patch('dataset_helper_object.requests.get')
     @patch('dataset_helper_object.EntitySdk.get_entity_by_id')
     @patch('dataset_helper_object.EntitySdk.get_ancestors')
-    @patch('dataset_helper_object.SearchSdk.assayname')
-    def test_verify_dataset_title_info_organ_code_not_found_in_types_file(self, mock_assayname, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
-        # https://github.com/hubmapconsortium/search-api/blob/main/src/search-schema/data/definitions/enums/assay_types.yaml
+    def test_verify_dataset_title_info_organ_code_not_found_in_types_file(self, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
         def resp1():
-            response_dict = {'description': 'Imaging Mass Cytometry', 'alt-names': [], 'primary': 'true', 'vitessce-hints': []}
-            return response_dict
-
-        def resp2():
-            response_dict = {'description': 'Bulk RNA-seq', 'alt-names': [], 'primary': 'true', 'vitessce-hints': []}
-            return response_dict
-        mock_assayname.side_effect = [resp1(), resp2()]
-
-        def resp3():
             r = requests.Response()
             r.status_code = 200
             dataset1 = hubmap_sdk.Dataset({'entity_type': 'Sample',
@@ -190,14 +152,14 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
                                            }})
             entity = [dataset1, dataset2]
             return entity
-        mock_get_ancestors.side_effect = [resp3()]
+        mock_get_ancestors.side_effect = [resp1()]
 
-        def resp4():
+        def resp2():
             dataset = hubmap_sdk.Dataset(self.dataset)
             return dataset
-        mock_get_entity_by_id.side_effect = [resp4()]
+        mock_get_entity_by_id.side_effect = [resp2()]
 
-        def resp5():
+        def resp3():
             r = requests.Response()
             json_data = '''
             {
@@ -209,13 +171,12 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
             '''
             r._content = json_data.encode('utf-8')
             return r
-        mock_url_get.side_effect = [resp5()]
+        mock_url_get.side_effect = [resp3()]
 
         result = self.dataset_helper.verify_dataset_title_info(self.dataset_uuid, self.user_token)
 
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0], "Organ code 'xx' not found in organ types file")
-        mock_assayname.assert_called()
         mock_get_ancestors.assert_called()
         mock_get_entity_by_id.assert_called()
         mock_url_get.assert_called()
@@ -223,19 +184,8 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
     @patch('dataset_helper_object.urllib.request.urlopen')
     @patch('dataset_helper_object.EntitySdk.get_entity_by_id')
     @patch('dataset_helper_object.EntitySdk.get_ancestors')
-    @patch('dataset_helper_object.SearchSdk.assayname')
-    def test_verify_dataset_title_info_organ_key_not_found(self, mock_assayname, mock_get_ancestors, mock_get_entity_by_id, mock_url_open):
-        # https://github.com/hubmapconsortium/search-api/blob/main/src/search-schema/data/definitions/enums/assay_types.yaml
+    def test_verify_dataset_title_info_organ_key_not_found(self, mock_get_ancestors, mock_get_entity_by_id, mock_url_open):
         def resp1():
-            response_dict = {'description': 'Imaging Mass Cytometry', 'alt-names': [], 'primary': 'true', 'vitessce-hints': []}
-            return response_dict
-
-        def resp2():
-            response_dict = {'description': 'Bulk RNA-seq', 'alt-names': [], 'primary': 'true', 'vitessce-hints': []}
-            return response_dict
-        mock_assayname.side_effect = [resp1(), resp2()]
-
-        def resp3():
             dataset1 = hubmap_sdk.Dataset({'entity_type': 'Sample',
                                'specimen_type': 'Organ',
                                'sample_category': 'organ',})
@@ -250,26 +200,25 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
 
             entity = [dataset1, dataset2]
             return entity
-        mock_get_ancestors.side_effect = [resp3()]
+        mock_get_ancestors.side_effect = [resp1()]
 
-        def resp4():
+        def resp2():
             dataset = hubmap_sdk.Dataset(self.dataset)
             return dataset
-        mock_get_entity_by_id.side_effect = [resp4()]
+        mock_get_entity_by_id.side_effect = [resp2()]
 
-        def resp5():
+        def resp3():
             r = requests.Response()
             r.read = lambda: b'AO:\r\n  description: Aorta\r\nBL:\r\n  \r\nBD:\r\n  description: Blood\r\nBM:\r\n  description: Bone Marrow\r\nBR:\r\n  description: Brain\r\n'
             # The 'when' needs the close method in the response....
             r.close = lambda: True
             return r
-        mock_url_open.side_effect = [resp5()]
+        mock_url_open.side_effect = [resp3()]
 
         result = self.dataset_helper.verify_dataset_title_info(self.dataset_uuid, self.user_token)
 
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0], 'Organ key not found in sample_category organ')
-        mock_assayname.assert_called()
         mock_get_ancestors.assert_called()
         mock_get_entity_by_id.assert_called()
         # because no 'organ:' is called by mock_get_ancestors
@@ -278,21 +227,8 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
     @patch('dataset_helper_object.requests.get')
     @patch('dataset_helper_object.EntitySdk.get_entity_by_id')
     @patch('dataset_helper_object.EntitySdk.get_ancestors')
-    @patch('dataset_helper_object.SearchSdk.assayname')
-    def test_verify_dataset_title_info_no_race_no_sex(self, mock_assayname, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
-        # https://github.com/hubmapconsortium/search-api/blob/main/src/search-schema/data/definitions/enums/assay_types.yaml
+    def test_verify_dataset_title_info_no_race_no_sex(self, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
         def resp1():
-            response_dict = {'description': 'Imaging Mass Cytometry', 'alt-names': [], 'primary': 'true',
-                             'vitessce-hints': []}
-            return response_dict
-
-        def resp2():
-            response_dict = {'description': 'Bulk RNA-seq', 'alt-names': [], 'primary': 'true', 'vitessce-hints': []}
-            return response_dict
-
-        mock_assayname.side_effect = [resp1(), resp2()]
-
-        def resp3():
             dataset1 = hubmap_sdk.Dataset({'entity_type': 'Sample',
                                            'specimen_type': 'Organ',
                                            'sample_category': 'organ',
@@ -307,14 +243,14 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
             entity = [dataset1, dataset2]
             return entity
 
-        mock_get_ancestors.side_effect = [resp3()]
+        mock_get_ancestors.side_effect = [resp1()]
 
-        def resp4():
+        def resp2():
             dataset = hubmap_sdk.Dataset(self.dataset)
             return dataset
-        mock_get_entity_by_id.side_effect = [resp4()]
+        mock_get_entity_by_id.side_effect = [resp2()]
 
-        def resp5():
+        def resp3():
             r = requests.Response()
             json_data = '''
             {
@@ -326,14 +262,13 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
             '''
             r._content = json_data.encode('utf-8')
             return r
-        mock_url_get.side_effect = [resp5()]
+        mock_url_get.side_effect = [resp3()]
 
         result = self.dataset_helper.verify_dataset_title_info(self.dataset_uuid, self.user_token)
 
         self.assertEqual(len(result), 2)
         self.assertEqual(result[0], f'Donor metadata.organ_donor_data grouping_concept_preferred_term race not found')
         self.assertEqual(result[1], f'Donor metadata.organ_donor_data grouping_concept_preferred_term sex not found')
-        mock_assayname.assert_called()
         mock_get_ancestors.assert_called()
         mock_get_entity_by_id.assert_called()
         mock_url_get.assert_called()
@@ -341,11 +276,8 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
     @patch('dataset_helper_object.requests.get')
     @patch('dataset_helper_object.EntitySdk.get_entity_by_id')
     @patch('dataset_helper_object.EntitySdk.get_ancestors')
-    @patch('dataset_helper_object.SearchSdk.assayname')
-    def test_verify_dataset_title_info_assaytype_not_found(self, mock_assayname, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
+    def test_verify_dataset_title_info_assaytype_not_found(self, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
         # https://github.com/hubmapconsortium/search-api/blob/main/src/search-schema/data/definitions/enums/assay_types.yaml
-
-        mock_assayname.side_effect = [Exception(), Exception()]
 
         def resp1():
             dataset1 = hubmap_sdk.Dataset({'entity_type': 'Sample',
@@ -389,10 +321,6 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
 
         result = self.dataset_helper.verify_dataset_title_info(self.dataset_uuid, self.user_token)
 
-        self.assertEqual(len(result), 2)
-        self.assertEqual(result[0], 'Unable to query the assay type details of: bulk-RNA via search-api')
-        self.assertEqual(result[1], 'Unable to query the assay type details of: IMC via search-api')
-        mock_assayname.assert_called()
         mock_get_ancestors.assert_called()
         mock_get_entity_by_id.assert_called()
         mock_url_get.assert_called()
@@ -400,20 +328,9 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
     @patch('dataset_helper_object.requests.get')
     @patch('dataset_helper_object.EntitySdk.get_entity_by_id')
     @patch('dataset_helper_object.EntitySdk.get_ancestors')
-    @patch('dataset_helper_object.SearchSdk.assayname')
-    def test_verify_dataset_title_info_dataset_data_types_missing(self, mock_assayname, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
+    def test_verify_dataset_title_info_dataset_data_types_missing(self, mock_get_ancestors, mock_get_entity_by_id, mock_url_get):
         # https://github.com/hubmapconsortium/search-api/blob/main/src/search-schema/data/definitions/enums/assay_types.yaml
         def resp1():
-            response_dict = {'description': 'Imaging Mass Cytometry', 'alt-names': [], 'primary': 'true', 'vitessce-hints': []}
-            return response_dict
-
-        def resp2():
-            response_dict = {'description': 'Bulk RNA-seq', 'alt-names': [], 'primary': 'true', 'vitessce-hints': []}
-            return response_dict
-
-        mock_assayname.side_effect = [resp1(), resp2()]
-
-        def resp3():
             dataset1 = hubmap_sdk.Dataset({'entity_type': 'Sample',
                                            'specimen_type': 'Organ',
                                            'sample_category': 'organ',
@@ -431,15 +348,15 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
 
             entity = [dataset1, dataset2]
             return entity
-        mock_get_ancestors.side_effect = [resp3()]
+        mock_get_ancestors.side_effect = [resp1()]
 
-        def resp4():
+        def resp2():
             entity = hubmap_sdk.Dataset({'uuid': self.dataset_uuid})
             return entity
 
-        mock_get_entity_by_id.side_effect = [resp4()]
+        mock_get_entity_by_id.side_effect = [resp2()]
 
-        def resp5():
+        def resp3():
             r = requests.Response()
             json_data = '''
             {
@@ -452,14 +369,13 @@ class TestVerifyDatasetTitleInfo(unittest.TestCase):
             r._content = json_data.encode('utf-8')
             return r
 
-        mock_url_get.side_effect = [resp5()]
+        mock_url_get.side_effect = [resp3()]
 
         result = self.dataset_helper.verify_dataset_title_info(self.dataset_uuid, self.user_token)
 
         self.assertEqual(len(result), 1)
-        self.assertEqual(result[0], 'The dataset did not contain a ''data_types'' key')
-        # Because dataset did not contain a ''data_types'' key...
-        mock_assayname.assert_not_called()
+        self.assertEqual(result[0], 'The dataset did not contain a ''dataset_type'' key')
+        # Because dataset did not contain a ''dataset_type'' key...
         mock_get_ancestors.assert_called()
         mock_get_entity_by_id.assert_called()
         mock_url_get.assert_called()
