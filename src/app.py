59708d7928e49d290e30651bf9d2985b423b310c
Lisa-Ann B <lisabruney@pitt.edu>
2024-03-19 10:13:53 -0400

    Reduce scope of processed datasets query Resolve globus url for processed datasets - #523

--- src/app.py
+++ src/app.py
@@ -2437,7 +2437,8 @@ def update_datasets_datastatus():
 
     processed_datasets_query = (
         "MATCH (s:Dataset)<-[:ACTIVITY_OUTPUT]-(a:Activity)<-[:ACTIVITY_INPUT]-(ds:Dataset) WHERE "
-                             "a.creation_action in ['Central Process', 'Lab Process'] RETURN DISTINCT ds.uuid AS uuid, COLLECT(DISTINCT s) AS processed_datasets"
+                             "a.creation_action in ['Central Process', 'Lab Process'] RETURN DISTINCT ds.uuid AS uuid, "
+        "COLLECT(DISTINCT {uuid: s.uuid, hubmap_id: s.hubmap_id, status: s.status, created_timestamp: s.created_timestamp, data_access_level: s.data_access_level, group_name: s.group_name}) AS processed_datasets"
     )
 
     upload_query = (
@@ -2532,6 +2533,9 @@ def update_datasets_datastatus():
                     dataset[prop] = ""
             if dataset[prop] is None:
                 dataset[prop] = ""
+            if prop == 'processed_datasets':
+                for processed in dataset['processed_datasets']:
+                    processed['globus_url'] = get_globus_url(processed.get('data_access_level'), processed.get('group_name'), processed.get('uuid'))
         for field in displayed_fields:
             if dataset.get(field) is None:
                 dataset[field] = ""
