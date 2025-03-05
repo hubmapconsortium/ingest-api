cf58e502f3408da950eec8da46f8e8471d9146ba
DerekFurstPitt <drf57@pitt.edu>
2025-03-05 16:51:00 -0500

    changed glob to iglob in file_exists

--- src/app.py
+++ src/app.py
@@ -3479,7 +3479,7 @@ def files_exist(uuid, data_access_level, group_name, metadata=False):
         if not metadata:
             return True
         else:
-            if any(glob.glob(os.path.join(file_path, '*metadata.tsv'))):
+            if any(glob.iglob(os.path.join(file_path, '*metadata.tsv'))):
                 return True
             else:
                 return False
